77768391b632e9413968f3b27e5d5bbd1f52c07d
Fabián Medina <fabianmedina09@gmail.com>
2022-12-13 22:18:32 -0500

    tested in windows

--- macys.js
+++ macys.js
@@ -3,36 +3,52 @@ import puppeteer from "puppeteer";
 (async () => {
   const browser = await puppeteer.launch({
     headless: false,
-    ignoreHTTPSErrors: true, 
+    ignoreHTTPSErrors: true,
+    executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
     args: [
-        '--disable-gpu',
-        '--disable-dev-shm-usage',
-        '--disable-setuid-sandbox',
-        '--no-first-run',
-        '--no-sandbox',
-        '--no-zygote',
-        '--single-process',
-        '--disable-features=AudioServiceOutOfProcess'
+      `--window-size=1550,1000`
     ],
+    defaultViewport: {
+      width: 1550,
+      height: 1000
+    }
   });
-  const page = await browser.newPage();
 
-  //   await page.goto("https://macys.com");
+  const page = await browser.newPage();
+  // await page.setViewport({ width: 1366, height: 768 });
+  await page.setExtraHTTPHeaders({
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
+    'upgrade-insecure-requests': '1',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+  })
+
   try {
-    await page.goto("https://macys.com/", {
-      waitUntil: "networkidle2",
-      timeout: 20000,
-    });
+    await page.goto('https://www.macys.com/', { waitUntil: 'networkidle2' });
   } catch (error) {
     console.log(error);
   }
 
+  await page.type('#globalSearchInputField', 'Calvin clein');
+  page.keyboard.press('Enter');
+
   console.log("before waiting");
-  await page.waitForTimeout(10000);
+  await page.waitForTimeout(5000);
   console.log("after waiting");
 
+
+  // await container.click()
+  const lis = await page.$$('ul.items li') // all <li> element handles within the ul
+  await lis[0].click()
+ 
+
+
+  console.log("before waiting");
+  await page.waitForTimeout(5000);
+  console.log("after waiting");
+  
+
   // Type into search box.
-  //   await page.type('.devsite-search-field', 'Headless Chrome');
+    
 
   //   // Wait for suggest overlay to appear and click "show all results".
   //   const allResultsSelector = '.devsite-suggest-all-results';
